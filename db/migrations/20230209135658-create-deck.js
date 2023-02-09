542b51ed2f0df6ea86b352805c8e1abf2ceba087
Anastasia <anastasia-vinogradova5@yandex.ru>
2023-02-09 18:58:24 +0300

    DB done

---
+++ db/migrations/20230209135658-create-deck.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Decks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER
+      },
+      theme: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Decks');
+  }
+};
