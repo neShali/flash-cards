91e2f9aad83bd4b49feacc61a9398c105cc3e294
Anton Shmelev <poshtet.spb@gmail.com>
2023-02-09 19:03:36 +0300

    login

--- routes/home.route.js
+++ routes/home.route.js
@@ -1,10 +1,25 @@
 const router = require('express').Router();
 const Home = require('../views/Home');
+// const User = require('../db/models/User')
 
 router.route('/')
   .get((req, res) => {
     res.renderComponent(Home, {
       userName: 'Nikita', themeOne: 'cat', themeTwo: 'krot', themeThree: 'scoth',
+    });
+  })
+  .post(async (req, res) => {
+    if (req.body.name && req.body.password) {
+      const findLogin = await User.findOne({ where: { name: req.body.name } });
+      if (!findLogin) {
+        await User.create({
+          name: req.body.name,
+          password: req.body.password,
+        });
+      } 
+    }
+    res.renderComponent(Home, {
+      userName: req.body.name, themeOne: 'cat', themeTwo: 'krot', themeThree: 'scoth',
     });
   });
 
